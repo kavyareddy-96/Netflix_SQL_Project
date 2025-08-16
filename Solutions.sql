b8393b983b1f179ac89aeb35cb7127e728923089
kavyareddy-96 <yellurukavya06@gmail.com>
2025-08-16 12:11:16 +0530

    Update Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -1,4 +1,5 @@
 -- Netfilx Project
+
 DROP TABLE IF EXISTS netflix;
 CREATE TABLE netflix
 (
@@ -21,25 +22,13 @@ SELECT* FROM netflix;
 SELECT 
      COUNT(*) as total_content
 FROM netflix;
-
-
-
 SELECT 
      *
 FROM netflix;
-
-
-
 SELECT 
     DISTINCT type
 FROM netflix;
-
-
 SELECT * FROM netflix;
-
-
-
-
 
 
 
@@ -53,12 +42,6 @@ SELECT
 	COUNT(*)
 FROM netflix
 GROUP BY type
-
-
-
-
-
-
 
 
 --2. Find the most common rating for movies and TV shows
@@ -85,12 +68,6 @@ SELECT
 FROM RankedRatings
 WHERE rank = 1;
 
-
-
-
-
-
-
 --3. List all movies released in a specific year (e.g., 2020)
 
 --filter 2020
@@ -98,9 +75,6 @@ WHERE rank = 1;
 SELECT * 
 FROM netflix
 WHERE release_year = 2020
-
-
-
 
 --4. Find the top 5 countries with the most content on Netflix
 
@@ -118,10 +92,6 @@ WHERE country IS NOT NULL
 ORDER BY total_content DESC
 LIMIT 5
 
-
-
-
-
 --5. Identify the longest movie
 
 SELECT 
@@ -130,22 +100,11 @@ FROM netflix
 WHERE type = 'Movie'
 ORDER BY SPLIT_PART(duration, ' ', 1)::INT DESC
 
-
-
-
-
-
 --6. Find content added in the last 5 years
 SELECT
 *
 FROM netflix
 WHERE TO_DATE(date_added, 'Month DD, YYYY') >= CURRENT_DATE - INTERVAL '5 years'
-
-
-
-
-
-
 
 --7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
 
@@ -163,12 +122,6 @@ netflix
 WHERE 
 	director_name = 'Rajiv Chilaka'
 
-
-
-
-
-
-
 --8. List all TV shows with more than 5 seasons
 
 SELECT *
@@ -178,13 +131,6 @@ WHERE
 	AND
 	SPLIT_PART(duration, ' ', 1)::INT > 5
 
-
-
-
-
-
-
-
 --9. Count the number of content items in each genre
 
 SELECT 
@@ -192,12 +138,6 @@ SELECT
 	COUNT(*) as total_content
 FROM netflix
 GROUP BY 1
-
-
-
-
-
-
 
 --10.Find each year and the average numbers of content release in India on netflix. 
 --return top 5 year with highest avg content release!
@@ -217,48 +157,21 @@ WHERE country = 'India'
 GROUP BY country, 2
 ORDER BY avg_release DESC 
 LIMIT 5
-
-
-
-
-
-
 --11. List all movies that are documentaries
 
 SELECT * FROM netflix
 WHERE listed_in LIKE '%Documentaries'
-
-
-
-
-
-
-
-
-
 --12. Find all content without a director
 
 SELECT * FROM netflix
 WHERE director IS NULL
 
-
-
-
-
-
 --13. Find how many movies actor 'Salman Khan' appeared in last 10 years!
-
-
 SELECT * FROM netflix
 WHERE 
 	casts LIKE '%Salman Khan%'
 	AND 
 	release_year > EXTRACT(YEAR FROM CURRENT_DATE) - 10
-
-
-
-
-
 
 --14. Find the top 10 actors who have appeared in the highest number of movies produced in India.
 SELECT 
@@ -269,14 +182,6 @@ WHERE country = 'India'
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 10
-
-
-
-
-
-
-
-
 --15.Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
   --the description field. Label content containing these keywords as 'Bad' and all other 
   --content as 'Good'. Count how many items fall into each category.
@@ -304,3 +209,4 @@ ORDER BY 2
 
 
   
+
